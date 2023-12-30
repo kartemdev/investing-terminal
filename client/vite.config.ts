75791514ec3b191197192e71180d649aca072fda
kartemdev <ihctuyd@mail.ru>
2023-12-30 07:28:31 +0300

    feat: IT-2 add npm deps, shared module, global types

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { viteBuild, vitePlugins, viteResolve } from './vite-modules';
 
 export default defineConfig({
+  base: '',
   plugins: vitePlugins,
   resolve: viteResolve,
   build: viteBuild,
